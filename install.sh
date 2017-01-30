42f89b347b8e93a7bf0b749a0b908b60811cb8c3
vorband <andre@vorbach.org>
2017-01-30 17:16:49 +0100

    Update for T-Pot 16.10 / Ubuntu 16.04

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 ##########################################################
-# T-Pot 16.03 install script                             #
-# Ubuntu server 14.04.04, x64                            #
+# T-Pot 16.10 install script                             #
+# Ubuntu server 16.04.0x, x64                            #
 #                                                        #
-# v1.0 by av, DTAG 2016-03-15                            #
+# v1.0 by av, DTAG 2017-01-30                            #
 #                                                        #
-# based on T-Pot 16.03 Community Edition Script          #
-# v16.03.14 by mo, DTAG, 2016-03-08                      #
+# based on T-Pot 16.10 Community Edition Script          #
+# v16.10.0 by mo, DTAG, 2016-12-03                       #
 ##########################################################
 
 
@@ -19,11 +19,21 @@ echo $1 "$2"
 tput setaf $myWHT
 }
 
+# used for hostname
+fuRANDOMWORD () {
+  local myWORDFILE=/usr/share/dict/names
+  local myLINES=$(cat $myWORDFILE  | wc -l)
+  local myRANDOM=$((RANDOM % $myLINES))
+  local myNUM=$((myRANDOM * myRANDOM % $myLINES + 1))
+  echo -n $(sed -n "$myNUM p" $myWORDFILE | tr -d \' | tr A-Z a-z)
+}
+
+
 fuECHO ""
 echo "
 ##########################################################
-# T-Pot 16.03 install script                             #
-# for Ubuntu server 14.04.04, x64                        #
+# T-Pot 16.10 install script                             #
+# for Ubuntu server 16.04.0x, x64                        #
 ##########################################################
 Make sure the SSH login for your normal user is working!
 "
@@ -52,7 +62,7 @@ fi
 
 # check if ssh daemon is running
 sshstatus=$(service ssh status)
-if [[ ! $sshstatus =~ "ssh start/running, process" ]];
+if [[ ! $sshstatus =~ "active (running)" ]];
 	then
 		echo "### SSH is not running. Script will abort!"
 		exit 1
@@ -80,10 +90,11 @@ if ! fgrep -q "PubkeyAuthentication yes" /etc/ssh/sshd_config
 		exit 1
 fi
 
-# check for ubuntu 14.04. distribution
-if ! fgrep  -q 'Ubuntu 14.04' /etc/issue
+# check for ubuntu 16.04. distribution
+release=$(lsb_release -r|cut -d $'\t' -f2)
+if [ $release != "16.04" ]
     then
-        fuECHO "### Wrong distribution. Must be Ubuntu 14.04.*. Script will abort! "
+        fuECHO "### Wrong distribution. Must be Ubuntu 16.04.*. Script will abort! "
         exit 1
 fi
 
@@ -94,6 +105,11 @@ if [ -f install.log ];
         exit 1
 fi
 
+# Let's log for the beauty of it
+set -e
+exec 2> >(tee "install.err")
+exec > >(tee "install.log")
+
 
 echo "Everything looks OK..."
 echo ""
@@ -102,24 +118,23 @@ echo "##########################################################"
 echo "#                                                        #"
 echo "#     How do you want to proceed? Enter your choice.     #"
 echo "#                                                        #"
-echo "# 1 - T-Pot's STANDARD INSTALLATION (w/o INDUSTRIAL)     #"
+echo "# 1 - T-Pot's STANDARD INSTALLATION                      #"
 echo "#     Requirements: >=4GB RAM, >=64GB disk               #"
 echo "#     Services: Cowrie, Dionaea, ElasticPot, Glastopf,   #"
-echo "#     Honeytrap, ELK, Suricata+P0f                       #"
+echo "#     Honeytrap, ELK & Suricata                          #"
 echo "#                                                        #"
 echo "# 2 - T-Pot's HONEYPOTS ONLY (w/o INDUSTRIAL)            #"
 echo "#     Requirements: >=3GB RAM, >=64GB disk               #"
 echo "#     Services:                                          #"
-echo "#     Cowrie, Dionaea, ElasticPot, Glastopf, Honeytrap   #"
+echo "#     Cowrie, Dionaea, ElasticPot, Glastopf & Honeytrap  #"
 echo "#                                                        #"
 echo "# 3 - T-Pot's INDUSTRIAL EDITION                         #"
-echo "#     Requirements: >=3GB RAM, >=64GB disk               #"
-echo "#     Services: ConPot, eMobility, ELK, Suricata+P0f     #"
+echo "#     Requirements: >=4GB RAM, >=64GB disk               #"
+echo "#     Services: ConPot, eMobility, ELK & Suricata        #"
 echo "#                                                        #"
 echo "# 4 - T-Pot's FULL INSTALLATION                          #"
 echo "#     Requirements: >=8GB RAM, >=128GB disk              #"
-echo "#     Services: Cowrie, Dionaea, ElasticPot, Glastopf,   #"
-echo "#     Honeytrap, ELK, Suricata+P0f                       #"
+echo "#     Services: Everything                               #"
 echo "#                                                        #"
 echo "##########################################################"
 echo ""
@@ -157,10 +172,49 @@ read choice
 
 # End checks
 
-# Let's log for the beauty of it
-set -e
-exec 2> >(tee "t-pot-error.log")
-exec > >(tee "t-pot-install.log")
+# Let's pull some updates
+fuECHO "### Pulling Updates."
+apt-get update -y
+fuECHO "### Installing Updates."
+apt-get upgrade -y
+
+# Install packages needed
+apt-get install apt-transport-https ca-certificates curl dialog dstat ethtool genisoimage git htop libpam-google-authenticator lm-sensors ntp openssh-server syslinux pv vim apache2-utils apparmor nginx aufs-tools bash-completion build-essential  cgroupfs-mount docker.io glances html2text iptables iw libltdl7 man nginx-extras nodejs npm ntp openssl psmisc python-pip -y 
+
+# Let's clean up apt
+apt-get autoclean -y
+apt-get autoremove -y
+
+# Let's remove NGINX default website
+fuECHO "### Removing NGINX default website."
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+rm /usr/share/nginx/html/index.html
+
+# Let's ask user for a password for the web user
+myOK="n"
+myUSER=$myuser
+fuECHO "### Please enter a password for your user $myuser for web access."
+myPASS1="pass1"
+myPASS2="pass2"
+while [ "$myPASS1" != "$myPASS2"  ] 
+  do
+    while [ "$myPASS1" == "pass1"  ] || [ "$myPASS1" == "" ]
+      do
+        read -s -p "Password: " myPASS1
+        fuECHO
+      done
+    read -s -p "Repeat password: " myPASS2
+    fuECHO
+    if [ "$myPASS1" != "$myPASS2" ];
+      then
+        fuECHO "### Passwords do not match."
+        myPASS1="pass1"
+        myPASS2="pass2"
+    fi
+  done
+htpasswd -b -c /etc/nginx/nginxpasswd $myUSER $myPASS1
+fuECHO
 
 # Let's modify the sources list
 sed -i '/cdrom/d' /etc/apt/sources.list
@@ -171,25 +225,20 @@ tee -a /etc/ssh/ssh_config <<EOF
 UseRoaming no
 EOF
 
-# Let's add the docker repository
-fuECHO "### Adding the docker repository."
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-tee /etc/apt/sources.list.d/docker.list <<EOF
-deb https://apt.dockerproject.org/repo ubuntu-trusty main
-EOF
+# Let's generate a SSL certificate
+fuECHO "### Generating a self-signed-certificate for NGINX."
+fuECHO "### If you are unsure you can use the default values."
+mkdir -p /etc/nginx/ssl
+openssl req -nodes -x509 -sha512 -newkey rsa:8192 -keyout "/etc/nginx/ssl/nginx.key" -out "/etc/nginx/ssl/nginx.crt" -days 3650
 
-# Let's pull some updates
-fuECHO "### Pulling Updates."
-apt-get update -y
-fuECHO "### Installing Updates."
-apt-get upgrade -y
+# Installing alerta-cli, wetty
+fuECHO "### Installing alerta-cli."
+pip install --upgrade pip
+pip install alerta
+fuECHO "### Installing wetty."
+ln -s /usr/bin/nodejs /usr/bin/node
+npm install https://github.com/t3chn0m4g3/wetty -g
 
-# Install packages needed
-apt-get install apt-transport-https ca-certificates curl dialog dstat ethtool genisoimage git htop libpam-google-authenticator lm-sensors ntp openssh-server syslinux pv vim  -y
-
-# Let's install docker
-fuECHO "### Installing docker-engine."
-apt-get install docker-engine=1.10.2-0~trusty -y
 
 # Let's add a new user
 fuECHO "### Adding new user."
@@ -198,7 +247,11 @@ adduser --system --no-create-home --uid 2000 --disabled-password --disabled-logi
 
 # Let's set the hostname
 fuECHO "### Setting a new hostname."
-myHOST=ce$(date +%s)$RANDOM
+myHOST=$(curl ‐s ‐f www.nsanamegenerator.com | html2text | tr A‐Z a‐z | awk '{print $1}')
+if [ "$myHOST" = "" ]; then
+  fuECHO "### Failed to fetch name from remote, using local cache."
+  myHOST=$(fuRANDOMWORD)
+fi
 hostnamectl set-hostname $myHOST
 sed -i 's#127.0.1.1.*#127.0.1.1\t'"$myHOST"'#g' /etc/hosts
 
@@ -207,16 +260,28 @@ fuECHO "### Patching sshd_config to listen on port 64295 and deny password authe
 sed -i 's#Port 22#Port 64295#' /etc/ssh/sshd_config
 sed -i 's#\#PasswordAuthentication yes#PasswordAuthentication no#' /etc/ssh/sshd_config
 
+# Let's allow ssh password authentication from RFC1918 networks
+fuECHO "### Allow SSH password authentication from RFC1918 networks"
+tee -a /etc/ssh/sshd_config <<EOF
+Match address 127.0.0.1,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16
+    PasswordAuthentication yes
+EOF
+
 # Let's patch docker defaults, so we can run images as service
 fuECHO "### Patching docker defaults."
 tee -a /etc/default/docker <<EOF
 DOCKER_OPTS="-r=false"
 EOF
 
+# Let's restart docker for proxy changes to take effect
+systemctl restart docker
+sleep 5
+
+
 # getting t-pot git repo
 fuECHO "### Cloning T-Pot Repository."
 cwdir=$(pwd)
-git clone https://github.com/dtag-dev-sec/tpotce.git -b 16.03
+git clone https://github.com/dtag-dev-sec/tpotce.git
 cp -R $cwdir/tpotce/installer/ $cwdir
 rm -rf $cwdir/tpotce/
 rm $cwdir/installer/install.sh $cwdir/installer/rc.local.install
@@ -248,20 +313,10 @@ esac
 
 # Let's load docker images
 fuECHO "### Loading docker images. Please be patient, this may take a while."
-if [ -d /root/tpot/images ];
-  then
-    fuECHO "### Found cached images and will load from local."
-    for name in $(cat /data/images.conf)
+for name in $(cat /data/images.conf)
     do
-      fuECHO "### Now loading dtagdevsec/$name:latest1603"
-      docker load -i /root/tpot/images/$name:latest1603.img
+      docker pull dtagdevsec/$name:latest1610
     done
-  else
-    for name in $(cat /data/images.conf)
-    do
-      docker pull dtagdevsec/$name:latest1603
-    done
-fi
 
 # Let's patch /etc/issue for t-pot autoinstall
 sed -i '14,15d' $cwdir/etc/issue
@@ -274,10 +329,6 @@ APT::Periodic::Update-Package-Lists "1";
 APT::Periodic::Download-Upgradeable-Packages "0";
 APT::Periodic::AutocleanInterval "7";
 EOF
-
-# Let's wait no longer for network than 60 seconds
-# fuECHO "### Wait no longer for network than 60 seconds."
-# sed -i.bak 's#sleep 60#sleep 30#' /etc/init/failsafe.conf
 
 # Let's make sure to reboot the system after a kernel panic
 fuECHO "### Reboot after kernel panic."
@@ -293,27 +344,40 @@ fuECHO "### Adding cronjobs."
 tee -a /etc/crontab <<EOF
 # Determine running containers every 120s
 */2 * * * * 	root 	/usr/bin/status.sh > /home/$myuser/docker-status
+
 # Check if containers and services are up
 */5 * * * * 	root 	/usr/bin/check.sh
+
+# Example for alerta‐cli IP update
+#*/5 * * * *	root	alerta --endpoint-url http://<ip>:<port>/api delete --filters resource=<host> && alerta --endpoint-url http://<ip>:<port>/api send -e IP -r <host> -E Production -s ok -S T-Pot -t \$(cat /data/elk/logstash/mylocal.ip) --status open
+
 # Check if updated images are available and download them
-27 1 * * *  	root	for i in \$(cat /data/images.conf); do /usr/bin/docker pull dtagdevsec/\$i:latest1603; done
+27 1 * * *  	root	for i in \$(cat /data/images.conf); do /usr/bin/docker pull dtagdevsec/\$i:latest1610; done
+
 # Restart docker service and containers
 27 3 * * * 	root 	/usr/bin/dcres.sh
+
 # Delete elastic indices older than 90 days
 27 4 * * *  root  /usr/bin/docker exec elk bash -c '/usr/local/bin/curator --host 127.0.0.1 delete indices --older-than 90 --time-unit days --timestring '%Y.%m.%d''
+
 # Update IP and erase check.lock if it exists
 27 15 * * * root /etc/rc.local
+
+# Daily reboot
+27 23 * * *	root	reboot
+
 # Check for updated packages every sunday, upgrade and reboot
-27 16 * * 0   root  apt-get autoclean -y; apt-get autoremove -y; apt-get update -y; apt-get upgrade -y; sleep 5; reboot
+27 16 * * 0   root  apt-get autoclean -y; apt-get autoremove -y; apt-get update -y; apt-get upgrade -y; sleep 10; reboot
 EOF
 
 # Let's create some files and folders
 fuECHO "### Creating some files and folders."
 mkdir -p /data/conpot/log \
          /data/cowrie/log/tty/ /data/cowrie/downloads/ /data/cowrie/keys/ /data/cowrie/misc/ \
-         /data/dionaea/log /data/dionaea/bistreams /data/dionaea/binaries /data/dionaea/rtp /data/dionaea/wwwroot \
+         /data/dionaea/log /data/dionaea/bistreams /data/dionaea/binaries /data/dionaea/rtp /data/dionaea/roots/ftp /data/dionaea/roots/tftp /data/dionaea/roots/www /data/dionaea/roots/upnp \
          /data/elasticpot/log \
-         /data/elk/data /data/elk/log /data/glastopf /data/honeytrap/log/ /data/honeytrap/attacks/ /data/honeytrap/downloads/ \
+         /data/elk/data /data/elk/log /data/elk/logstash/conf \
+         /data/glastopf /data/honeytrap/log/ /data/honeytrap/attacks/ /data/honeytrap/downloads/ \
          /data/emobility/log \
          /data/ews/log /data/ews/conf /data/ews/dionaea /data/ews/emobility \
          /data/suricata/log /home/$myuser/.ssh/
@@ -324,54 +388,80 @@ chmod 500 $cwdir/bin/*
 chmod 600 $cwdir/data/*
 chmod 644 $cwdir/etc/issue
 chmod 755 $cwdir/etc/rc.local
-chmod 700 $cwdir/home/*
-chown $myuser:$myuser $cwdir/home/*
-chmod 644 $cwdir/data/upstart/*
-
+chmod 644 $cwdir/data/systemd/*
 
 # Let's copy some files
 tar xvfz $cwdir/data/elkbase.tgz -C /
 cp $cwdir/data/elkbase.tgz /data/
 cp -R $cwdir/bin/* /usr/bin/
 cp -R $cwdir/data/* /data/
-cp -R $cwdir/etc/issue /etc/
-cp -R $cwdir/home/* /home/$myuser/
+cp    $cwdir/data/systemd/* /etc/systemd/system/
+cp    $cwdir/etc/issue /etc/
+cp -R $cwdir/etc/nginx/ssl /etc/nginx/
+cp    $cwdir/etc/nginx/tpotweb.conf /etc/nginx/sites-available/
+cp    $cwdir/etc/nginx/nginx.conf /etc/nginx/nginx.conf
+cp    $cwdir/usr/share/nginx/html/* /usr/share/nginx/html/
+
 for i in $(cat /data/images.conf);
   do
-    cp /data/upstart/$i.conf /etc/init/;
+    systemctl enable $i;
 done
+systemctl enable wetty
 
-# Let's turn persistence off by default
-touch /data/persistence.off
+# Let's enable T-Pot website
+fuECHO "### Enabling T-Pot website."
+ln -s /etc/nginx/sites-available/tpotweb.conf /etc/nginx/sites-enabled/tpotweb.conf
 
 # Let's take care of some files and permissions
 chmod 760 -R /data
 chown tpot:tpot -R /data
 chmod 600 /home/$myuser/.ssh/authorized_keys
-chown $myuser:$myuser /home/$myuser/*.sh /home/$myuser/.ssh /home/$myuser/.ssh/authorized_keys
+chown $myuser:$myuser /home/$myuser/.ssh /home/$myuser/.ssh/authorized_keys
 
-# Let's clean up apt
-apt-get autoclean -y
-apt-get autoremove -y
-
-# we already have ssh enabled. so we have to remove this option from ~/2fa_enable.sh.
-sed -i '34,45d;16,18d' /home/$myuser/2fa_enable.sh
+# Let's replace "quiet splash" options, set a console font for more screen canvas and update grub
+sed -i 's#GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"#GRUB_CMDLINE_LINUX_DEFAULT="consoleblank=0"#' /etc/default/grub
+sed -i 's#GRUB_CMDLINE_LINUX=""#GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"#' /etc/default/grub
+#sed -i 's#\#GRUB_GFXMODE=640x480#GRUB_GFXMODE=800x600x32#' /etc/default/grub
+#tee -a /etc/default/grub <<EOF
+#GRUB_GFXPAYLOAD=800x600x32
+#GRUB_GFXPAYLOAD_LINUX=800x600x32
+#EOF
+update-grub
+cp /usr/share/consolefonts/Uni2-Terminus12x6.psf.gz /etc/console-setup/
+gunzip /etc/console-setup/Uni2-Terminus12x6.psf.gz
+sed -i 's#FONTFACE=".*#FONTFACE="Terminus"#' /etc/default/console-setup
+sed -i 's#FONTSIZE=".*#FONTSIZE="12x6"#' /etc/default/console-setup
+update-initramfs -u
 
 # Let's enable a color prompt
-sed -i 's#\#force_color_prompt=yes#force_color_prompt=yes#' /home/$myuser/.bashrc
-sed -i 's#\#force_color_prompt=yes#force_color_prompt=yes#' /root/.bashrc
+myROOTPROMPT='PS1="\[\033[38;5;8m\][\[$(tput sgr0)\]\[\033[38;5;1m\]\u\[$(tput sgr0)\]\[\033[38;5;6m\]@\[$(tput sgr0)\]\[\033[38;5;4m\]\h\[$(tput sgr0)\]\[\033[38;5;6m\]:\[$(tput sgr0)\]\[\033[38;5;5m\]\w\[$(tput sgr0)\]\[\033[38;5;8m\]]\[$(tput sgr0)\]\[\033[38;5;1m\]\\$\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]"'
+myUSERPROMPT='PS1="\[\033[38;5;8m\][\[$(tput sgr0)\]\[\033[38;5;2m\]\u\[$(tput sgr0)\]\[\033[38;5;6m\]@\[$(tput sgr0)\]\[\033[38;5;4m\]\h\[$(tput sgr0)\]\[\033[38;5;6m\]:\[$(tput sgr0)\]\[\033[38;5;5m\]\w\[$(tput sgr0)\]\[\033[38;5;8m\]]\[$(tput sgr0)\]\[\033[38;5;2m\]\\$\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]"'
+tee -a /root/.bashrc << EOF
+$myROOTPROMPT
+EOF
+tee -a /home/$myuser/.bashrc << EOF
+$myUSERPROMPT
+EOF
 
 # Let's create ews.ip before reboot and prevent race condition for first start
 myLOCALIP=$(hostname -I | awk '{ print $1 }')
 myEXTIP=$(curl myexternalip.com/raw)
 sed -i "s#IP:.*#IP: $myLOCALIP, $myEXTIP#" /etc/issue
+sed -i "s#SSH:.*#SSH: ssh -l $myuser -p 64295 $myLOCALIP#" /etc/issue
+sed -i "s#WEB:.*#WEB: https://$myLOCALIP:64297#" /etc/issue
+
 tee /data/ews/conf/ews.ip << EOF
 [MAIN]
 ip = $myEXTIP
 EOF
+echo $myLOCALIP > /data/elk/logstash/mylocal.ip
 chown $myuser:$myuser /data/ews/conf/ews.ip
 
+# change user for wetty 
+sed -i 's/tsec/'$myuser'/' /etc/systemd/system/wetty.service
+sed -i 's/tsec/'$myuser'/' /usr/share/nginx/html/navbar.html
+systemctl daemon-reload
 
 # Final steps
-fuECHO "### Thanks for your patience. Now rebooting. Remember to login on SSH port 64295 next time!"
+fuECHO "### Thanks for your patience. Now rebooting. Remember to login on SSH port 64295 next time or visit dashboard at port 64297!"
 mv $cwdir/etc/rc.local /etc/rc.local && rm -rf $cwdir && sleep 2 &&reboot
